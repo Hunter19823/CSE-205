4f74389131b0c7fe38c3c9bbb8b7bbee28ba1e3f
Hunter19823 <hspragg@asu.edu>
2021-02-09 10:50:05 -0700

    Uploaded Push Counter for Week 4.

---
+++ src/projects/week6/guis/PushCounterPanel.java
@@ -0,0 +1,37 @@
+package projects.week6.guis;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PushCounterPanel extends JPanel {
+    private int count;
+    private JButton push;
+    private JLabel label;
+
+    public PushCounterPanel()
+    {
+        count = 0;
+
+        push = new JButton("Push Me");
+        push.addActionListener(new ButtonListener());
+
+        label = new JLabel();
+        label.setText("Pushes: "+count);
+
+        add(push);
+        add(label);
+
+        setBackground(Color.cyan);
+        setPreferredSize(new Dimension(300,40));
+    }
+
+    private class ButtonListener implements ActionListener{
+        public void actionPerformed(ActionEvent event)
+        {
+            count++;
+            label.setText("Pushes: "+count);
+        }
+    }
+}
