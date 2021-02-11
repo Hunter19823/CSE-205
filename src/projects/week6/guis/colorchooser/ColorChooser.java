7ef3c9560e32ca33c69e5b6cfb182f1437f00d05
Hunter19823 <hspragg@asu.edu>
2021-02-11 11:23:28 -0700

    Addeded Week4, session 2's example files.

---
+++ src/projects/week6/guis/colorchooser/ColorChooser.java
@@ -0,0 +1,22 @@
+package projects.week6.guis.colorchooser;
+
+import javax.swing.*;
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * Author: Hunter S.
+ */
+public class ColorChooser {
+    public static void main(String[] args)
+    {
+
+        JFrame frame = new JFrame("Display File");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JColorChooser chooser = new JColorChooser();
+
+        frame.getContentPane().add(chooser);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
